1b510632e3a07c0a77fe5996358c7c819ca1d77d
Sunny Prakash <sunny.prakashgm@gmail.com>
2020-04-19 23:34:10 +0530

    added drag-drop funtionality

--- decorators/app.js
+++ decorators/app.js
@@ -1,14 +1,27 @@
+"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-//A decorator in a function you apply to a class.
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+/* ---> 1. class Decorator*/
 //it will be called when js will find the definiton of your constuctor not when that will be used
-function Logger(constructor) {
-    console.log('logging...');
-    console.log('Loger-->', constructor);
+function Logger(msg) {
+    return function (constructor) {
+        console.log('LOGGER FUNCTION MESSAGE ' + msg);
+    };
+}
+function WithTemplate(template, id) {
+    return function (constructor) {
+        const p = new constructor();
+        const rootEl = document.getElementById(id);
+        if (rootEl)
+            rootEl.innerHTML = `<h1>${template} for ${p.name}</h1>`;
+    };
 }
 let Person = class Person {
     constructor() {
@@ -17,7 +30,82 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    Logger
+    Logger('LOGGING - PERSON'),
+    WithTemplate('Person Object', 'root')
 ], Person);
-const sunny = new Person();
-console.log(sunny);
+/* ---> 2. Property Decorator*/
+/* ---> 3. Accessor Decorator*/
+/* ---> 4. Method Decorator*/
+/* ---> 5. Parameter Decorator -> It wont work for accesors*/
+function LogProps(target, propName) {
+    console.log(target, propName);
+}
+function LogAccessors(target, name, descriptor) {
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function LogMethod(target, name, descriptor) {
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function LogParams(target, name, position) {
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+class Product {
+    constructor(t, p) {
+        this._title = t;
+        this._price = p;
+    }
+    getPiceWithTax(tax) {
+        return this._price + tax;
+    }
+    get title() {
+        return this._title;
+    }
+    set price(value) {
+        if (value > 0)
+            this._price = value;
+        else
+            throw new Error('Price should be greater than 0');
+    }
+}
+__decorate([
+    LogProps //it runs when class is registered with javaScript not when instance of class will be created
+], Product.prototype, "_title", void 0);
+__decorate([
+    LogMethod,
+    __param(0, LogParams)
+], Product.prototype, "getPiceWithTax", null);
+__decorate([
+    LogAccessors
+], Product.prototype, "title", null);
+/* ---> 6. Modify descriptor of methods*/
+function Autobind(_, _2, descriptor) {
+    const boundMethod = descriptor.value;
+    const newDescription = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            return boundMethod.bind(this);
+        }
+    };
+    return newDescription;
+}
+class Printer {
+    constructor() {
+        this.msg = 'this works';
+    }
+    print() {
+        console.log(this.msg);
+    }
+}
+__decorate([
+    Autobind
+], Printer.prototype, "print", null);
+const p = new Printer();
+const btn = document.querySelector('button');
+btn.addEventListener('click', p.print);
--- decorators/app.ts
+++ decorators/app.ts
@@ -1,11 +1,21 @@
-//A decorator in a function you apply to a class.
+/* ---> 1. class Decorator*/
 //it will be called when js will find the definiton of your constuctor not when that will be used
-function Logger(constructor: Function) {
-    console.log('logging...');
-    console.log('Loger-->', constructor);
+function Logger(msg: string) {
+    return function (constructor: Function) {
+        console.log('LOGGER FUNCTION MESSAGE ' + msg);
+    }
 }
 
-@Logger
+function WithTemplate(template: string, id: string) {
+    return function (constructor: any) {
+        const p = new constructor();
+        const rootEl = document.getElementById(id);
+        if (rootEl) rootEl.innerHTML = `<h1>${template} for ${p.name}</h1>`;
+    }
+}
+
+@Logger('LOGGING - PERSON')
+@WithTemplate('Person Object', 'root')
 class Person {
     private name = 'sunny';
     constructor() {
@@ -13,5 +23,79 @@ class Person {
     }
 }
 
-const sunny = new Person();
-console.log(sunny)
+/* ---> 2. Property Decorator*/
+/* ---> 3. Accessor Decorator*/
+/* ---> 4. Method Decorator*/
+/* ---> 5. Parameter Decorator -> It wont work for accesors*/
+function LogProps(target: any, propName: string | Symbol) {
+    console.log(target, propName);
+}
+
+function LogAccessors(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function LogMethod(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function LogParams(target: any, name: string | Symbol, position: number) {
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+
+class Product {
+    @LogProps//it runs when class is registered with javaScript not when instance of class will be created
+    private _title: string;
+    private _price: number
+    constructor(t: string, p: number) {
+        this._title = t;
+        this._price = p;
+    }
+    @LogMethod
+    getPiceWithTax(@LogParams tax: number) {
+        return this._price + tax;
+    }
+    @LogAccessors
+    get title() {
+        return this._title
+    }
+
+    set price(value: number) {
+        if (value > 0) this._price = value
+        else throw new Error('Price should be greater than 0');
+    }
+}
+
+/* ---> 6. Modify descriptor of methods*/
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const boundMethod = descriptor.value;
+    const newDescription: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            return boundMethod.bind(this);
+        }
+    }
+    return newDescription;
+}
+
+class Printer {
+    private msg = 'this works'
+
+    @Autobind
+    print() {
+        console.log(this.msg);
+    }
+}
+
+const p = new Printer();
+
+const btn = document.querySelector('button')!;
+btn.addEventListener('click', p.print)
